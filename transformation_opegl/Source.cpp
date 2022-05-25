ba7ea0b5f1916aacc113a2033f45c13d37e5ba19
Anastasiia Savchenko <anastasiiaolegivna02@gmail.com>
2022-05-26 02:05:03 +0300

    Add project files.

---
+++ transformation_opegl/Source.cpp
@@ -0,0 +1,134 @@
+//#include "pch.h"
+
+// C code to implement basic
+// transformations in OPENGL
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+#include <GL/glut.h>
+
+// Window size
+#define maxWD 640
+#define maxHT 480
+
+// Rotation speed
+#define thetaSpeed 0.05
+
+// This creates delay between
+// two actions
+void delay(unsigned int mseconds)
+{
+	clock_t goal = mseconds + clock();
+	while (goal > clock());
+}
+
+// This is a basic init for the
+// glut window
+void myInit(void)
+{
+	glClearColor(1.0, 1.0,
+		1.0, 0.0);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(0.0, maxWD,
+		0.0, maxHT);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glFlush();
+}
+
+// This function just draws
+// a point
+void drawPoint(int x, int y)
+{
+	glPointSize(7.0);
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glBegin(GL_POINTS);
+	glVertex2i(x, y);
+	glEnd();
+}
+
+void rotateAroundPt(int px, int py,
+	int cx, int cy)
+{
+	float theta = 0.0;
+	while (1)
+	{
+		glClear(GL_COLOR_BUFFER_BIT);
+		int xf, yf;
+
+		// оновленн€ тети
+		theta = theta + thetaSpeed;
+
+		// перев≥рка надлишковост≥
+		if (theta >= (2.0 * 3.14159))
+			theta = theta - (2.0 * 3.14159);
+
+		// матриц€ повороту..
+		xf = (cx + (int)((float)(px - cx) *
+			cos(theta)) - ((float)(py - cy) *
+				sin(theta)));
+		yf = (cy + (int)((float)(px - cx) *
+			sin(theta)) + ((float)(py - cy) *
+				cos(theta)));
+
+		// центральна точка
+		drawPoint(cx, cy);
+
+		// точка дл€ обертанн€
+		drawPoint(xf, yf);
+		glFlush();
+		delay(10);
+	}
+}
+
+void translatePoint(int px, int py, int tx, int ty)
+{
+	int fx = px, fy = py;
+	while (1)
+	{
+		glClear(GL_COLOR_BUFFER_BIT);
+		px = px + tx;
+		py = py + ty;
+		if (px > maxWD || px < 0 ||
+			py > maxHT || py < 0)
+		{
+			px = fx;
+			py = fy;
+		}
+		drawPoint(px, py);
+
+		glFlush();
+		delay(10);
+	}
+}
+
+void myDisplay(void)
+{
+	int opt;
+	printf("\nEnter\n\t<1> for translation"
+		"\n\t<2> for rotation\t:");
+	scanf_s("%d", &opt);
+	printf("\nGo to the window...");
+	switch (opt) {
+	case 1:
+		translatePoint(100, 200, 1, 5);
+		break;
+	case 2:
+		rotateAroundPt(200, 200, maxWD / 2,
+			maxHT / 2);
+		break;
+	}
+}
+void main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowSize(maxWD, maxHT);
+	glutInitWindowPosition(100, 150);
+	glutCreateWindow("OpenGL");
+
+	glutDisplayFunc(myDisplay);
+	myInit();
+	glutMainLoop();
+}
+
